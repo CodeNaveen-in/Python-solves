1a99788b68735a656da276ab7f6df3c80222e5ff
Naveen Garg <nvngrg0102@gmail.com>
2025-07-28 15:26:15 +0530

    String and List Update 1.1

--- tools_set/high_Power.py
+++ tools_set/high_Power.py
@@ -8,4 +8,23 @@ def printIncreasingPower(x):
             print ( i*i, end=' ')
         i+=1
         #code here
-printIncreasingPower(100)
+#printIncreasingPower(100)
+
+def decrementList(arr):
+    #code here
+    for i in arr:
+        print(int(i-1), end=" ")
+
+arr = [54, 43, 2, 1, 5]
+#decrementList(arr)
+
+def isPalindrome(s):
+    for i in range (len(s)):
+        if (s[i] == s[-(i+1)]):
+            p = True
+        else:
+            p = False
+            break
+    return p
+
+isPalindrome("abba")
